742cdc9b7810104092874fe192dd804968745902
XDarkArcher <azala.justice@gmail.com>
2020-01-30 11:42:49 +0000

    Simple Data Exploration

--- Beijing Air Quality.R
+++ Beijing Air Quality.R
@@ -1,12 +1,42 @@
 #Loading in the Dataset 
-folder <- "C:/Users/User/Documents/University/Honours Project/Air Quality/"
- file_list <- list.files(path = folder, pattern = "*.csv")
+folder <- "C:/Users/User/Documents/Honours-Project/Air Quality/"
+file_list <- list.files(path = folder, pattern = "*.csv")
 
  for (i in 1:length(file_list)){
    assign(file_list[i], 
    read.csv(paste(folder, file_list[i], sep=''))
    )}
  
- #Simple Data Exploration 
- nrow(Aotizhongxin_Station.csv)
+#Simple Data Exploration 
+
+#Get the number of Rows and Columns
+nrow(Aotizhongxin_Station.csv)
+ncol(Aotizhongxin_Station.csv)
+
+#Get the column names  
+names(Aotizhongxin_Station.csv)
+ 
+#Show the first 10 rows in the dataframe
+Aotizhongxin_Station.csv[0:9,]
+
+#Summary of the Aotizhongxin monotoring station 
+summary(Aotizhongxin_Station.csv)
+
+#Counts all the missing values for each Column
+colSums(is.na(Aotizhongxin_Station.csv))
+colSums(is.na(Changping_Station.csv))
+colSums(is.na(Dingling_Station.csv))
+colSums(is.na(Dongsi_Station.csv))
+colSums(is.na(Guanyuan_Station.csv))
+colSums(is.na(Gucheng_Station.csv))
+colSums(is.na(Huairou_Station.csv))
+colSums(is.na(Nongzhanguan_Station.csv))
+colSums(is.na(Shunyi_Station.csv))
+colSums(is.na(Tiantan_Station.csv))
+colSums(is.na(Wanliu_Station.csv))
+colSums(is.na(Wanshouxigong_Station.csv))
+
+#Simple Visualisation of current trend 
+
+
  
